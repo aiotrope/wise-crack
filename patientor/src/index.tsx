aeb1739a70d7de469dfdb6a490faef70d85b2246
aiotrope <arneli74@protonmail.com>
2022-12-24 12:07:28 +0200

    💎 style(styles): edit files

--- patientor/src/index.tsx
+++ patientor/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
-import './_index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
